140ce0618a11a4ccda51f6984ee300e0e0a0e0db
Gajanan Taur <Gajanan Taur>
2025-05-30 14:17:38 +0530

    Initial commit

---
+++ calculator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+
+echo "--- Menu ---"
+echo "0. Exit"
+echo "------------"
+
+read -p "Enter choice: " choice
+
+case $choice in
+    0)
+        echo "Exiting..."
+        exit 0
+        ;;
+    
+esac
